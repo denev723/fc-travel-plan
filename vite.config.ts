6114c88be0caa41746db67a7ec5be75f6485739c
denev <denev723@gmail.com>
2025-04-02 22:25:26 +0900

    refactof: 경로 별칭 및 TypeScript 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPath from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPath()],
   root: "client",
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
   server: {
     proxy: {
       "/api": "http://localhost:3000",
