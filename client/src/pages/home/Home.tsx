faae7d6cd503d6fa99582659b7920faefdfa89b8
denev <denev723@gmail.com>
2025-04-10 22:10:33 +0900

    feat: 모달 컴포넌트 추가

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import Loading from "@/components/common/Loading";
+import Modal, { ModalBackdrop, ModalPanel } from "@/components/common/Modal";
 import NarrowLayout from "@/components/common/NarrowLayout";
 import CityList from "@/components/home/CityList";
 import FilterList from "@/components/home/FilterList";
 import SearchInput from "@/components/home/SearchInput";
 import { getCities, getSearchedCities } from "@/services/home";
+import { useModalStore } from "@/store";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
@@ -15,17 +17,35 @@ export default function Home() {
     queryFn: q ? () => getSearchedCities(q) : getCities,
   });
 
+  const { openModal } = useModalStore();
+
+  const handleClick = () => {
+    openModal(({ onClose }) => (
+      <Modal>
+        <ModalBackdrop />
+        <ModalPanel>
+          <div className="bg-white">
+            <button onClick={onClose}>닫기</button>
+          </div>
+        </ModalPanel>
+      </Modal>
+    ));
+  };
+
   return isLoading || !data ? (
     <Loading />
   ) : (
-    <NarrowLayout className="my-30 flex flex-col items-center">
-      <div className="w-[339px] mb-24">
-        <SearchInput onCompositionEnd={(value) => setQ(value)} />
-      </div>
-      <div className="mb-21">
-        <FilterList active="all" onChange={() => {}} />
-      </div>
-      <CityList cities={data} />
-    </NarrowLayout>
+    <>
+      <button onClick={handleClick}>open modal</button>
+      <NarrowLayout className="my-30 flex flex-col items-center">
+        <div className="w-[339px] mb-24">
+          <SearchInput onCompositionEnd={(value) => setQ(value)} />
+        </div>
+        <div className="mb-21">
+          <FilterList active="all" onChange={() => {}} />
+        </div>
+        <CityList cities={data} />
+      </NarrowLayout>
+    </>
   );
 }
